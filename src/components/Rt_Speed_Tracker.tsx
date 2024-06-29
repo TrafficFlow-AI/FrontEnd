cb027d27484e7a45b2b3167f0909cb3af88768dc
Jlarrinzal <jose.maria.larrinzal@estudiantat.upc.edu>
2024-06-29 19:13:26 +0200

    fix: url video

--- src/components/Rt_Speed_Tracker.tsx
+++ src/components/Rt_Speed_Tracker.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import {
     Table,
     TableBody,
@@ -41,13 +43,22 @@ import {
   ];
   
   export function Rt_Speed_Tracker() {
+    const [imageSrc, setImageSrc] = useState("http://10.0.2.52:5000/speed_estimation");
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setImageSrc(`http://127.0.0.1:5000/speed_estimation?timestamp=${new Date().getTime()}`);
+      }, 5000); // Actualiza cada 5 segundos
+  
+      return () => clearInterval(interval);
+    }, []);
     return (
       <div className="max-w-4xl mx-auto">
         <h2 className="font-semibold mb-4 text-center">Real Time Speed Tracker</h2>
         <div className="flex flex-col sm:flex-row justify-between items-center">
           <div className="flex justify-center items-center mb-4 sm:mb-0 sm:mr-8">
             <img
-              src="https://www.canal26.com/media/image/2021/12/14/493487.jpg"
+              src={imageSrc}
               alt="Real time video"
               className="max-w-full"
             />

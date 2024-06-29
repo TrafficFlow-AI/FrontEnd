22c76361278c236c66c386386a59cb61d90062ea
paugarcia32 <paugarcia32@gmail.com>
2024-06-29 16:38:13 +0200

    feat: navbar done

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center font-semibold">
+      <h1>hola</h1>
+    </main>
+  )
+}
